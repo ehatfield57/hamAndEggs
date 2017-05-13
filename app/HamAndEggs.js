13060a7389a4c85a1701d0e94e2205954c58b21a
ehatfield57@gmail.com <ehatfield57@gmail.com>
2017-05-13 14:20:19 -0700

    First test

---
+++ app/HamAndEggs.js
@@ -0,0 +1,9 @@
+class HamAndEggs {
+  constructor(operator, words, total) {
+    this.op = operator;
+    this.words = words;
+    this.total = total;
+  }
+}
+
+module.exports = HamAndEggs;
--- spec/hamAndEggs.spec.js
+++ spec/hamAndEggs.spec.js
@@ -1,5 +1,8 @@
-describe('Test hamAndEggs puzzler', () => {
+const HamAndEggs = require('../app/HamAndEggs.js');
+
+describe('Test HamAndEggs puzzler', () => {
   it('should create a hamAndEggs instance', () => {
-    expect(true).toBe(true);
+    let hamAndEggs = new HamAndEggs('+', ['HAM', 'AND'], 'EGGS');
+    expect(hamAndEggs instanceof HamAndEggs).toBe(true);
   });
 });
