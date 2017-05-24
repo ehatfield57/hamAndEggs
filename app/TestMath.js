842ff2c30d67308143355d1edd8079e8d7a49616
ehatfield57@gmail.com <ehatfield57@gmail.com>
2017-05-23 19:15:17 -0700

    Got the TestMath working using simpler means

--- app/TestMath.js
+++ app/TestMath.js
@@ -1,55 +1,30 @@
 
 class TestMath {
   constructor(op, words, total) {
-    this.functions = [];
-    this.setup(words.map( word => [...word] ), [...total], op);
-  }
-
-  setup(words, total, op) {
-    while (true) {
-      let letters = [];
-      for (let x=0; x < words.length; x++) {
-        let letter = words[x].splice(-1, 1);
-        if (letter.length > 0) {
-          letters.push(letter[0]);
-        }
-      }
-
-      if (letters.length === 0) {
-        break;
-      }
-
-      let totalLetter = total.splice(-1, 1);
-
-      this.functions.push((settings, carry=0) => {
-        console.log('Hi Edward, letters:', letters);
-        console.log('Hi Edward, totalLetter:', totalLetter);
-        console.log('Hi Edward, settings:', settings);
-        console.log('Hi Edward, op:', op);
-
-        return (given) => {
-          console.log('Hi Edward, given:', given);
-          let runningTotal = 0;
-          letters.forEach( letter => {
-            console.log('Hi Edward, letter:', letter);
-          });
-          return true;
-        };
-      });
-    }
+    this.words = words;
+    this.operator = op;
+    this.total = total;
   }
 
   test(settings) {
-    for( let func of this.functions) {
-      console.log('Hi Edward, func:', func);
-      let results = func(settings);
-      console.log('Hi Edward, results:', results);
-      if (! results) {
-        console.log('Hi Edward, func:', func, ', settings:', settings);
-        return false;
-      }
+    let numbers = this.words.map( word => parseInt( word.split('').map( letter => settings[letter] ).join('') ));
+    let total = parseInt( this.total.split('').map( letter => settings[letter] ).join('') );
+
+    let testTotal = 0;
+    switch( this.operator ) {
+      case('+'):
+        testTotal = numbers.reduce( (num, running) => running += num, numbers.pop() );
+        break;
+      case('-'):
+        testTotal = numbers.reduce( (num, running) => running -= num, numbers.pop() );
+        break;
+      default:
+        console.error(`I dont know how to do ${this.operator} yet`);
+        exit;
+        break;
     }
-    return true;
+
+    return total === testTotal;
   }
 }
 
--- spec/TestMath.spec.js
+++ spec/TestMath.spec.js
@@ -8,7 +8,13 @@ describe('TestMath', () => {
 
   it('should test a function', () => {
     let testMath = new TestMath('+', ['A', 'A'], 'B');
-    expect(testMath.test([{'A': 1, 'B': 2}])).toBe(true);
-    expect(testMath.test([{'A': 1, 'B': 3}])).toBe(false);
+    expect(testMath.test({'A': 1, 'B': 2})).toBe(true);
+    expect(testMath.test({'A': 1, 'B': 3})).toBe(false);
+
+    testMath = new TestMath('+', ['ABC', 'CBA'], 'DDD');
+    expect(testMath.test({'A': 1, 'B': 2, 'C': 3, 'D': 4})).toBe(true);
+
+    testMath = new TestMath('-', ['DDD', 'ABC'], 'CBA');
+    expect(testMath.test({'A': 1, 'B': 2, 'C': 3, 'D': 4})).toBe(true);
   });
 });
